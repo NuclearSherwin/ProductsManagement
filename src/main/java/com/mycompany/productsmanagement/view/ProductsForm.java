c9db4b40c48fb971cf729984cfaff9a6ac0834ec
paika2060 <paika2060@gmail.com>
2022-08-26 08:39:05 +0700

    adjust to mvc structure

--- src/main/java/com/mycompany/productsmanagement/view/ProductsForm.java
+++ src/main/java/com/mycompany/productsmanagement/view/ProductsForm.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.mycompany.productsmanagement;
+package com.mycompany.productsmanagement.view;
 
 import com.mycompany.productsmanagement.model.Product;
 import com.mycompany.productsmanagement.model.ProductList;
